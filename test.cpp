eecf56eba0fd78d5f25a84c7a916f97fbef3bc31
Mathieu GUIGUE <mgguigue@gmail.com>
2019-01-27 17:20:10 +0900

    Two wrenches

--- test.cpp
+++ test.cpp
@@ -8,8 +8,10 @@ int main()
     std::cout << "main" << std::endl;
     ToolChain toolchain;
     auto wrench = toolchain.CreateTool("Wrench", "splash");
+    auto wrench2 = toolchain.CreateTool("Wrench", "bing");
     auto screwDriver = toolchain.CreateTool("ScrewDriver", "paff");
     wrench->makeNoise();
+    wrench2->makeNoise();
     screwDriver->makeNoise();
     return 0;
 }
